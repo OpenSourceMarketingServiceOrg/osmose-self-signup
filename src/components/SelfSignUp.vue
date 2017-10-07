0576d20c9ed0c2f351739926ab8b84e386922065
Danjenator <danjenator@gmail.com>
2017-10-07 17:53:08 -0500

    started on self sign up component

---
+++ src/components/SelfSignUp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="signup">
+    <h1>{{title}}</h1>
+    <input v-model="message" placeholder="edit me">
+    <p>Message is: {{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelfSignUp',
+  data () {
+    return {
+      title: 'This is my title'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+a {
+  color: #42b983;
+}
+</style>
