8103eb0afd28a7f6a253c3e30a4e543d17d58d6a
zabiri <zachbiri@gmail.com>
2017-11-04 21:19:31 -0500

    For real this time

--- src/components/SelfSignUp.vue
+++ src/components/SelfSignUp.vue
@@ -1,35 +1,35 @@
 <template>
+  
   <div id='signup'>
-    <h1>{{title}}</h1>
-    <form novalidate v-on:submit.prevent='adduser'>
-      First Name: <br>
-      <input v-model.lazy.trim='user.fname' placeholder='first name'><br>
-      Last Name: <br>
-      <input v-model.lazy.trim='user.lname' placeholder='last name'><br>
-      Email: <br>
-      <input v-model.lazy.trim='user.email' placeholder='email@address.com'> <br>
-      <input type='submit' value='Submit'>
+    <h1 class="h-serif">{{title}}</h1>
+    <form novalidate v-on:submit.prevent='adduser' name="self_signup" class="col s12">
+      <div class="input-field col s6">
+        <input v-model.lazy.trim='user.fname' id="first_name" type="text" class="p-sans">
+        <label class="p-sans" for="first_name">First Name</label>
+      </div>
+      <div class="input-field col s6">
+        <input v-model.lazy.trim='user.lname' id="last_name" type="text" class="p-sans">
+        <label class="p-sans" for="last_name">Last Name</label>
+      </div>
+      <div class="input-field col s6">
+        <input v-model.lazy.trim='user.email' id="email_address" type="email" name="email" class="validate p-sans">
+        <label for="email_address" class="p-sans">Email Address</label>
+      </div>
+      <button type="submit" value="Submit" class="waves-effect waves-light btn os-btn">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import VueResource from 'vue-resource'
-import Api from '../mixins/api'
-Vue.use(VueResource)
-
 export default {
-  name: 'self-signup',
   data: () => ({
-    title: ' This is my Title!',
+    title: 'OSMOSe Self-Signup',
     user: {
       fname: '',
       lname: '',
       email: ''
     }
   }),
-  mixins: [Api],
   computed: {
     users () {
       console.log('computed called')
@@ -39,24 +39,16 @@ export default {
   methods: {
     adduser () {
       console.log('user: ', this.user)
-      console.log('api: ', Api.post)
-      this.$http.post(Api.post, this.user).then(res => {
-        console.log('res: ', res)
-      },
-      err => {
-        console.log('err: ', err)
-      })
       // return this.$store.commit('addUser', JSON.parse(JSON.stringify(this.user)));
     }
   }
 }
 </script>
 
-<style scoped>
-h1 {
-  font-weight: normal;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+@import '../assets/style/settings.scss';
+
+
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './plugins/vendor'
 
 Vue.config.productionTip = false
 
